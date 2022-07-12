0412785d3926b1bdb721296ebd9937267f746fa6
Etcaqab <Etcaqab@users.noreply.github.com>
2022-07-13 00:39:19 +0200

    Fix low node parent counting on node copy and destruction

--- src/mcts/node.h
+++ src/mcts/node.h
@@ -367,6 +367,11 @@ class Node {
         terminal_type_(Terminal::NonTerminal),
         lower_bound_(GameResult::BLACK_WON),
         upper_bound_(GameResult::WHITE_WON) {}
+  ~Node() { UnsetLowNode(); }
+
+  // Copy that updates low_node_ using UnsetLowNode/SetLowNode and moves
+  // sibling_ to target.
+  Node& operator=(Node&& n);
 
   // Allocates a new edge and a new node. The node has to be without edges
   // before that.
